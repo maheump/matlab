ec8287ee393a80e67de73c0672dde602d65cbf37
Maxime Maheu <maheu.mp@gmail.com>
2018-05-31 09:54:32 +0200

    Improve generality of the "pat2str" function

--- sequences/pat2str.m
+++ sequences/pat2str.m
@@ -1,4 +1,4 @@
-function str = pat2str( pat, letters )
+function str = pat2str( pat, letters, stims )
 %PAT2STR converts an array into a string (e.g. [2 1 2], [1 0 1] or [4 -1 4]
 %are returned as BAB).
 %
@@ -10,13 +10,18 @@ function str = pat2str( pat, letters )
 %
 %Copyright 2016 Maxime Maheu
 
+
+% Find the stimuli
+if nargin < 3
+    stims = unique(pat);
+end
+
 % Define the entire alphabet
 if nargin < 2
     letters = cellfun(@char, num2cell('A'+(1:26)-1), 'UniformOutput', false);
 end
 
-% Find the stimuli
-stims = unique(pat);
+% Check
 if numel(stims) > numel(letters)
     error('There is not enough letters to describe the pattern.');
 end
